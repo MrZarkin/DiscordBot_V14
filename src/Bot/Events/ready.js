dc3da387b5dee5ae570f54208c53d599828a8070
MrZarkin <matryck@outlook.fr>
2025-02-06 23:19:27 +0100

    Adding/Updating Events

--- src/Bot/Events/ready.js
+++ src/Bot/Events/ready.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
+const loadSlashCommands = require('../Loaders/loadSlashCommands');
 
-module.exports = () => {
+module.exports = async bot => {
+    
+    await loadSlashCommands(bot);
     console.log("Bot ready !")
 }
